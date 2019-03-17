49e66878596223a8f2c175427ddaaf9764e989b5
Rhian Advincula <rhian.dlra97@gmail.com>
2019-03-17 16:21:30 -0700

    Beginning 6 Categories

---
+++ js/results.js
@@ -0,0 +1,13 @@
+<div class="radial-graph">
+	<div class="shape">
+		<div class="mask full-mask">
+			<div class="fill"></div>
+		</div>
+		<div class="mask">
+			<div class="fill"></div>
+			<div class="fill shim"></div>
+		</div>
+	</div>
+	<div class="cutout">
+	</div>
+</div>
